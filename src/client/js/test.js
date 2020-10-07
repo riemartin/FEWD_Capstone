a7eac95a92906c75cc5c10d1ce8de00990c6c3d8
riemartin <64641306+riemartin@users.noreply.github.com>
2020-10-07 23:08:38 +0200

    server test implemented

--- src/client/js/test.js
+++ src/client/js/test.js
@@ -1,7 +1,17 @@
+// test client code
+
 import { uiUpdater } from './ui'
  
 describe("Testing the update functionality", () => {
     test("Testing the uiUpdater() function", () => {
            expect(uiUpdater).toBeDefined();
     }); 
+});
+
+// test server code
+
+const listening = require('../../server/server.js');
+
+test('listening', () => {
+    expect(listening).toBeDefined();
 });
